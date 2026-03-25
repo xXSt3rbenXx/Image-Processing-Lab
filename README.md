0d9fe24962b4e550d62a47d907cad9aae3e25471
xXSt3rbenXx <giorrossi03@gmail.com>
2026-03-25 18:05:36 +0100

    Completamento setup iniziale

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Image-Processing-Lab
+
+ È possibile trovare il dataset utilizzato in questo progetto al link "https://ieee-dataport.org/open-access/indian-diabetic-retinopathy-image-dataset-idrid"
+ La relazione del presente progetto è reperibile qui: "https://www.overleaf.com/read/byyzvwdrckyz#b891b9"
---
+++ main.m
@@ -0,0 +1,16 @@
+
+folder = '.\IDRiD\A. Segmentation\1. Original Images\a. Training Set';
+
+files = dir(fullfile(folder, '*.jpg')); % cambia estensione se serve
+
+for k = 1:length(files)
+    filename = fullfile(folder, files(k).name);
+    img = imread(filename);
+    pause(0.5); % per vedere le immagini una dopo l'altra
+end
+
+
+igray=rgb2gray(img)
+i_equalized=adapthisteq(igray) %applica CLAHE
+imshowpair(igray, i_equalized, 'montage')
+title('Originale vs CLAHE')
