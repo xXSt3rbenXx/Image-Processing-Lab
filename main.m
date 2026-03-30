db0c9042eb515be41c581ed64187831040d041b0
Giuseppe Pansini <giuseppepansini074@gmail.com>
2026-03-30 10:06:45 +0200

    Update main.m
    Segmentazione semplice da DOCUMENTARE

--- main.m
+++ main.m
@@ -19,7 +19,7 @@ for k = 1:2
     %PROVA CON SIGMA=50
     background=imgaussfilt(greenChannelImg, 50);
     figure(3)
-    imshow(background)
+    plot(background)
     title('Grafico filtro Gaussiano Largo con sigma=50')
 
     figure(4)
@@ -35,15 +35,17 @@ for k = 1:2
     
 end
 
-%ESPERIMENTO, UTILIZZO DI THRESHOLDING INVECE DI TRASHOLD ADATTIVA
-f=im2double(greenChannelEqualized);
-T1=0.5*mean(f(:));
+%ESPERIMENTO, UTILIZZO DI THRESHOLDING INVECE DI THRESHOLDING ADATTIVO
+imgThreshSimple=im2double(greenChannelEqualized);
+T1=0.5*mean(imgThreshSimple(:));
 done=false;
 while ~ done
-    g= f>=T1;
-    TNext=0.5*(mean(f(g))+mean(f( ~ g)));
+    g=imgThreshSimple>=T1;
+    TNext=0.5*(mean(imgThreshSimple(g))+mean(imgThreshSimple(~g)));
     done= abs(T1-TNext)<0.5;
     T1=TNext;
 end 
-BW=imbinarize(f,TNext);
-imshowpair(f,BW, 'montage')
+imgThreshSimple=imbinarize(imgThreshSimple,TNext);
+figure(6)
+imshow(imgThreshSimple)
+title('Immagine Segmentata con Tresholding')
