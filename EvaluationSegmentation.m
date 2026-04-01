a6bd763028cd112a19c8dfd8d6319edd4325415f
Giuseppe Pansini <giuseppepansini074@gmail.com>
2026-04-01 16:03:23 +0200

    SegmentationEvaluation
    Ho implementato Accuracy e Dice, ma ci sono devi valori abbastanza bassi -> BISOGNA RITORNARE SUL PREPROCESSING

---
+++ EvaluationSegmentation.m
@@ -0,0 +1,29 @@
+function metrics = evaluationSegmentation(segmentedIter, segmentedOtsu, gt)
+pred_iter=logical(segmentedIter);
+pred_otsu=logical(segmentedOtsu);
+gt=logical(gt);
+%converte valori numerici in array logici (booleani), 
+% dove i valori diversi da zero diventano true () e
+%  gli zeri diventano false ()
+
+%CALCOLO TN, TP, FP, FN
+TP_iter = sum(pred_iter(:) & gt(:));
+%LI TRASFORMO IN VETTORI COLONNA 
+TN_iter = sum(~pred_iter(:) & ~gt(:));
+FP_iter = sum(pred_iter(:) & ~gt(:));
+FN_iter = sum(~pred_iter(:) & gt(:));
+
+%TP, TN, FP, FN DI OTSU
+TP_otsu = sum(pred_otsu(:) & gt(:));
+%LI TRASFORMO IN VETTORI COLONNA 
+TN_otsu = sum(~pred_otsu(:) & ~gt(:));
+FP_otsu = sum(pred_otsu(:) & ~gt(:));
+FN_otsu = sum(~pred_otsu(:) & gt(:));
+
+metrics.iter.accuracy=(TP_iter+TN_iter)/(TP_iter+FN_iter+FP_iter+TN_iter);
+metrics.otsu.accuracy=(TP_otsu+TN_otsu)/(TP_otsu+FN_otsu+FP_otsu+TN_otsu);
+
+
+metrics.iter.dice=(2*TP_iter)/(2*TP_iter+FP_iter+FN_iter+eps);
+metrics.otsu.dice=(2*TP_otsu)/(2*TP_otsu+FP_otsu+FN_otsu+eps);
+end
